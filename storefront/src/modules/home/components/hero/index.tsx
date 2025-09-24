5852872c5161562be88016376beae369c00be69a
Ab Cd <smerch10128@gmail.com>
2025-09-24 22:22:15 +0300

    v10

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,45 +1,81 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 import { Suspense } from "react"
-import SkeletonProductGrid from "@modules/skeletons/templates/skeleton-product-grid"
-import PaginatedProducts from "@modules/store/templates/paginated-products"
-const Hero = () => {
-  return (
 
-<div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Well done1111! You have successfully deployed your Medusa 2.0 store on Railway!
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Need help customizing your store?
-          </Heading>
-        </span>
-        <a
-          href="https://funkyton.com/medusajs-2-0-is-finally-here/"
-          target="_blank"
-        >
-          <h1 style={{ textDecoration: "underline" }}>
-            Visit the tutorial
-          </h1>
-        </a>
+import SkeletonProductGrid from "@modules/skeletons/templates/skeleton-product-grid"
+import RefinementList from "@modules/store/components/refinement-list"
+import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
+
+import PaginatedProducts from "./paginated-products"
+
+// const Hero = () => {
+//   return (
+//     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+//       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+//         <span>
+//           <Heading
+//             level="h1"
+//             className="text-3xl leading-10 text-ui-fg-base font-normal"
+//           >
+//             Well done1111! You have successfully deployed your Medusa 2.0 store on Railway!
+//           </Heading>
+//           <Heading
+//             level="h2"
+//             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+//           >
+//             Need help customizing your store?
+//           </Heading>
+//         </span>
+//         <a
+//           href="https://funkyton.com/medusajs-2-0-is-finally-here/"
+//           target="_blank"
+//         >
+//           <h1 style={{ textDecoration: "underline" }}>
+//             Visit the tutorial
+//           </h1>
+//         </a>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Hero
+
+
+
+
+const StoreTemplate = ({
+  sortBy,
+  page,
+  countryCode,
+}: {
+  sortBy?: SortOptions
+  page?: string
+  countryCode: string
+}) => {
+  const pageNumber = page ? parseInt(page) : 1
+  const sort = sortBy || "created_at"
+
+  return (
+    <div
+      className="flex flex-col small:flex-row small:items-start py-6 content-container"
+      data-testid="category-container"
+    >
+      <RefinementList sortBy={sort} />
+      <div className="w-full">
+        <div className="mb-8 text-2xl-semi">
+          <h1 data-testid="store-page-title">All products</h1>
+        </div>
+        <Suspense fallback={<SkeletonProductGrid />}>
+          <PaginatedProducts
+            sortBy={sort}
+            page={pageNumber}
+            countryCode={countryCode}
+          />
+        </Suspense>
       </div>
     </div>
-  
-
-
-
-    
-
-)
+  )
 }
 
-export default Hero
-
+export default StoreTemplate
