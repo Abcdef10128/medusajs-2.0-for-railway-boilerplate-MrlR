11f346dfc19b0646bdaeaa987c465ff269bf143a
Ab Cd <smerch10128@gmail.com>
2025-09-25 23:07:55 +0300

    v11

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -73,8 +73,8 @@ const Hero = () => {
           <CarouselContent className="w-full">
             {Array.from({ length: 5 }).map((_, index) => (
               <CarouselItem key={index} className="w-full">
-                <Card className="w-full aspect-video">
-                  <CardContent className="flex aspect-square items-center justify-center p-6 w-full">
+                <Card className="w-full">
+                  <CardContent className="flex aspect-video items-center justify-center p-6 w-full">
                     <span className="text-4xl font-semibold">{index + 1}</span>
                   </CardContent>
                 </Card>
