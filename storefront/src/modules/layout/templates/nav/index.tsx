ab89b5f0590e5f0a295ef2c2fc62b5a6d30710fa
Ab Cd <smerch10128@gmail.com>
2025-09-26 21:03:41 +0300

    v11

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -23,99 +23,99 @@ export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
 
   return (
-    <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
-        <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
-          <div className="flex-1 basis-0 h-full flex items-center">
-            <div className="h-full">
-              <SideMenu regions={regions} />
-            </div>
-          </div>
+    <Dialog>
+      <form>
 
-          <div className="flex items-center h-full">
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-              data-testid="nav-store-link"
-            >
-              Medusa Store
-            </LocalizedClientLink>
-          </div>
+      
+        <div className="sticky top-0 inset-x-0 z-50 group">
+          <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
+            <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
+              <div className="flex-1 basis-0 h-full flex items-center">
+                <div className="h-full">
+                  <SideMenu regions={regions} />
+                </div>
+              </div>
 
-          <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-            <div className="hidden small:flex items-center gap-x-6 h-full">
-              <Dialog>
-                <form>
-                  <DialogTrigger asChild>
+              <div className="flex items-center h-full">
+                <LocalizedClientLink
+                  href="/"
+                  className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
+                  data-testid="nav-store-link"
+                >
+                  Medusa Store
+                </LocalizedClientLink>
+              </div>
+
+              <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
+                <div className="hidden small:flex items-center gap-x-6 h-full">
+
+                    
+                      <DialogTrigger asChild>
+                        <LocalizedClientLink
+                          className="hover:text-ui-fg-base"
+                          href="#"
+                          data-testid="nav-account-link"
+                        >
+                          Ціни
+                        </LocalizedClientLink>
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>Edit profile</DialogTitle>
+                          <DialogDescription>
+                            Make changes to your profile here. Click save when you&apos;re
+                            done.
+                          </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4">
+
+                        </div>
+                        <DialogFooter>
+                          <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                          </DialogClose>
+                          <Button type="submit">Save changes</Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    
+
+
+                  {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
                     <LocalizedClientLink
                       className="hover:text-ui-fg-base"
-                      href="#"
-                      data-testid="nav-account-link"
+                      href="/search"
+                      scroll={false}
+                      data-testid="nav-search-link"
                     >
-                      Ціни
+                      Search
                     </LocalizedClientLink>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                      <DialogTitle>Edit profile</DialogTitle>
-                      <DialogDescription>
-                        Make changes to your profile here. Click save when you&apos;re
-                        done.
-                      </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-4">
-
-                    </div>
-                    <DialogFooter>
-                      <DialogClose asChild>
-                        <Button variant="outline">Cancel</Button>
-                      </DialogClose>
-                      <Button type="submit">Save changes</Button>
-                    </DialogFooter>
-                  </DialogContent>
-                </form>
-              </Dialog>
-              <LocalizedClientLink
-                className="hover:text-ui-fg-base"
-                href="/account"
-                data-testid="nav-account-link"
-              >
-                Ціни
-              </LocalizedClientLink>
-              {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
-                <LocalizedClientLink
-                  className="hover:text-ui-fg-base"
-                  href="/search"
-                  scroll={false}
-                  data-testid="nav-search-link"
+                  )}
+                  <LocalizedClientLink
+                    className="hover:text-ui-fg-base"
+                    href="/account"
+                    data-testid="nav-account-link"
+                  >
+                    Account
+                  </LocalizedClientLink>
+                </div>
+                <Suspense
+                  fallback={
+                    <LocalizedClientLink
+                      className="hover:text-ui-fg-base flex gap-2"
+                      href="/cart"
+                      data-testid="nav-cart-link"
+                    >
+                      Cart (0)
+                    </LocalizedClientLink>
+                  }
                 >
-                  Search
-                </LocalizedClientLink>
-              )}
-              <LocalizedClientLink
-                className="hover:text-ui-fg-base"
-                href="/account"
-                data-testid="nav-account-link"
-              >
-                Account
-              </LocalizedClientLink>
-            </div>
-            <Suspense
-              fallback={
-                <LocalizedClientLink
-                  className="hover:text-ui-fg-base flex gap-2"
-                  href="/cart"
-                  data-testid="nav-cart-link"
-                >
-                  Cart (0)
-                </LocalizedClientLink>
-              }
-            >
-              <CartButton />
-            </Suspense>
-          </div>
-        </nav>
-      </header>
-    </div>
+                  <CartButton />
+                </Suspense>
+              </div>
+            </nav>
+          </header>
+        </div>
+      </form>
+    </Dialog>
   )
 }
