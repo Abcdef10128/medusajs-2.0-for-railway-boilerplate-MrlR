cd7126a6420b60a5deb5c08934601d49a4368ef8
Ab Cd <smerch10128@gmail.com>
2025-09-26 23:28:44 +0300

    v12

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -9,7 +9,24 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@lib/components/ui/dialog"
+import { AppWindowIcon, CodeIcon } from "lucide-react"
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@lib/components/ui/card"
+import { Input } from "@lib/components/ui/input"
+import { Label } from "@lib/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@lib/components/ui/tabs"
 
 import { Suspense } from "react"
 
@@ -47,7 +64,13 @@ export default async function Nav() {
               <Dialog>
                 <form>
                   <DialogTrigger asChild>
-                    <Button variant="outline">Open Dialog</Button>
+                    <LocalizedClientLink
+                      className="hover:text-ui-fg-base"
+                      href="#"
+                      data-testid="nav-account-link"
+                    >
+                      Ціни
+                    </LocalizedClientLink>
                   </DialogTrigger>
                   <DialogContent className="sm:max-w-[425px]">
                     <DialogHeader>
@@ -59,6 +82,62 @@ export default async function Nav() {
                     </DialogHeader>
                     <div className="grid gap-4">
 
+                    </div>
+                    <div className="flex w-full max-w-sm flex-col gap-6">
+                      <Tabs defaultValue="account">
+                        <TabsList>
+                          <TabsTrigger value="account">Account</TabsTrigger>
+                          <TabsTrigger value="password">Password</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="account">
+                          <Card>
+                            <CardHeader>
+                              <CardTitle>Account</CardTitle>
+                              <CardDescription>
+                                Make changes to your account here. Click save when you&apos;re
+                                done.
+                              </CardDescription>
+                            </CardHeader>
+                            <CardContent className="grid gap-6">
+                              <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-name">Name</Label>
+                                <Input id="tabs-demo-name" defaultValue="Pedro Duarte" />
+                              </div>
+                              <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-username">Username</Label>
+                                <Input id="tabs-demo-username" defaultValue="@peduarte" />
+                              </div>
+                            </CardContent>
+                            <CardFooter>
+                              <Button>Save changes</Button>
+                            </CardFooter>
+                          </Card>
+                        </TabsContent>
+                        <TabsContent value="password">
+                          <Card>
+                            <CardHeader>
+                              <CardTitle>Password</CardTitle>
+                              <CardDescription>
+                                Change your password here. After saving, you&apos;ll be logged
+                                out.
+                              </CardDescription>
+                            </CardHeader>
+                            <CardContent className="grid gap-6">
+                              <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-current">Current password</Label>
+                                <Input id="tabs-demo-current" type="password" />
+                              </div>
+                              <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-new">New password</Label>
+                                <Input id="tabs-demo-new" type="password" />
+                              </div>
+                            </CardContent>
+                            <CardFooter>
+                              <Button>Save password</Button>
+                            </CardFooter>
+                          </Card>
+                        </TabsContent>
+                      </Tabs>
                     </div>
                     <DialogFooter>
                       <DialogClose asChild>
