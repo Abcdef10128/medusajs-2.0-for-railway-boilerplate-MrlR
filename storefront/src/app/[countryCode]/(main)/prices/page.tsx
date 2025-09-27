7fa100ac474f19830adcd6486104736449e94615
Ab Cd <smerch10128@gmail.com>
2025-09-27 22:17:17 +0300

    v12

--- storefront/src/app/[countryCode]/(main)/prices/page.tsx
+++ storefront/src/app/[countryCode]/(main)/prices/page.tsx
@@ -104,7 +104,7 @@ export default function Prices() {
 const [pricesOpen, setPricesOpen] = useState(true)
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
+    <div className="grid place-items-center h-[100vh] w-full">
       <Card className="p-6">
                      <form>
 
