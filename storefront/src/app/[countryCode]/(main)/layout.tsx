739e2a845747e56741040aa7c7e38673b5562d92
Ab Cd <smerch10128@gmail.com>
2025-09-27 23:03:04 +0300

    v12

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default async function PageLayout(props: { children: React.ReactNode, modal: React.ReactNode }) {
   return (
-    <div className="pt-6">
+    <div className="pt-[150px]">
       <Nav />
       {props.children}
       {props.modal}
