d9a7dd23db0bcff094071727ca3185efa81ca2b7
Ab Cd <smerch10128@gmail.com>
2025-09-27 15:26:53 +0300

    v12

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -35,12 +35,14 @@ const Hero = () => {
       setCurrent(api.selectedScrollSnap() + 1)
     })
   }, [api])
+    const [pricesOpen, setPricesOpen] = React.useState(false)
   return (
         
     <div className="w-full">
-      <Prices open={false} onOpenChange={function (open: boolean): void {
-        throw new Error("Function not implemented.")
-      } } />
+      <Prices 
+        open={pricesOpen} 
+        onOpenChange={setPricesOpen} 
+      />
       {/* Hero секция */}
       {/* <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
         <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
