43ac27ebf06c6f3ad202ab6eeb6d51a737eaf2b0
Ab Cd <smerch10128@gmail.com>
2025-09-27 21:54:57 +0300

    v12

--- storefront/src/app/[countryCode]/(main)/prices/page.tsx
+++ storefront/src/app/[countryCode]/(main)/prices/page.tsx
@@ -104,7 +104,7 @@ export default function Prices() {
 const [pricesOpen, setPricesOpen] = useState(true)
 
   return (
-    <div className="fixed grid w-full place-items-center inset-0">
+    <div className="relative grid w-full place-items-center inset-0">
       <Card>
                      <form>
 
