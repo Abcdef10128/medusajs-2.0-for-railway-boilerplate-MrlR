05c05b7ca1b21efe5a055a717091179c8e0457ae
Ab Cd <smerch10128@gmail.com>
2025-09-27 14:07:38 +0300

    v12

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -6,11 +6,19 @@ export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
 
-export default function RootLayout(props: { children: React.ReactNode }) {
+export default function RootLayout(props: { 
+  children: React.ReactNode ,
+  prices: React.ReactNode
+
+} ) {
   return (
     <html lang="en" data-mode="light">
       <body>
-        <main className="relative">{props.children}</main>
+        <main className="relative">
+          {props.children}
+
+          {props.prices}
+        </main>
       </body>
     </html>
   )
