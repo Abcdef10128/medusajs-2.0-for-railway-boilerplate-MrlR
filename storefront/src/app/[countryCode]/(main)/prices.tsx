197bbf66e2e5253e9e4312f1bd9b27637e570116
Ab Cd <smerch10128@gmail.com>
2025-09-28 15:07:57 +0300

    v12

--- storefront/src/app/[countryCode]/(main)/prices.tsx
+++ storefront/src/app/[countryCode]/(main)/prices.tsx
@@ -113,7 +113,7 @@ const [pricesOpen, setPricesOpen] = useState(true)
                              <TabsTrigger value="account">Account</TabsTrigger>
                              <TabsTrigger value="password">Password</TabsTrigger>
                            </TabsList>
-                           <TabsContent value="account" className="gap-4">
+                           <TabsContent value="account" className="flex flex-colgap-4">
                              
                                {/* <CardContent> */}
                                  <Table className="border-3">
