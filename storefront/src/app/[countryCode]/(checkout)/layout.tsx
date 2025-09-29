3173dacf6e4013ff87d0681e16d6e085ab65de74
Ab Cd <smerch10128@gmail.com>
2025-09-29 23:43:06 +0300

    v12

--- storefront/src/app/[countryCode]/(checkout)/layout.tsx
+++ storefront/src/app/[countryCode]/(checkout)/layout.tsx
@@ -26,7 +26,7 @@ export default function CheckoutLayout({
           </LocalizedClientLink>
           <LocalizedClientLink
             href="/"
-            className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base flex flex-col gap-1"
+            className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base flex items-center gap-1"
             data-testid="store-link"
           >
             <img src="/logo_vector_png.png" className="size-9" alt="" />
