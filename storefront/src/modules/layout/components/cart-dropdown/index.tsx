9cfe3a5ea597aac4d6c47ba7b99dcd28d35ef6df
Ab Cd <smerch10128@gmail.com>
2025-09-29 23:20:07 +0300

    v12

--- storefront/src/modules/layout/components/cart-dropdown/index.tsx
+++ storefront/src/modules/layout/components/cart-dropdown/index.tsx
@@ -95,7 +95,7 @@ const CartDropdown = ({
         >
           <Popover.Panel
             static
-            className="hidden small:block absolute top-[calc(100%+1px)] right-0 bg-white border-x border-b border-gray-200 w-[450px] text-ui-fg-base"
+            className="hidden small:block absolute top-[calc(100%+1px)] right-0 bg-white border-x border-b border-gray-200 w-[420px] text-ui-fg-base"
             data-testid="nav-cart-dropdown"
           >
             <div className="p-4 flex items-center justify-center">
@@ -112,7 +112,7 @@ const CartDropdown = ({
                     })
                     .map((item) => (
                       <div
-                        className="grid grid-cols-[122px_1fr] gap-x-4"
+                        className="grid grid-cols-[122px_1fr] gap-x-2"
                         key={item.id}
                         data-testid="cart-item"
                       >
