b6f21670ec46f77bfcbcbf834a37101e9d70a761
Ab Cd <smerch10128@gmail.com>
2025-10-08 00:22:02 +0300

    v12

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -54,26 +54,13 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                 leaveFrom="opacity-100 backdrop-blur-2xl"
                 leaveTo="opacity-0"
               >
+ 
                 <Popover.Panel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
                   <div
                     data-testid="nav-menu-popup"
-                    className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6 overflow-hidden" // Добавьте overflow-hidden
-                    style={{
-                      position: 'fixed', // Фиксированная позиция
-                      top: '8px', // Соответствует m-2 (0.5rem = 8px)
-                      left: '8px',
-                      right: '8px',
-                      bottom: '8px',
-                      height: 'calc(100vh - 16px)', // Учитывая отступы
-                      overscrollBehavior: 'contain' // Предотвращает прокрутку за пределы
-                    }}
-                    >
-                {/* <Popover.Panel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
-                  <div
-                    data-testid="nav-menu-popup"
-                    className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6 overflow-hidden"
+                    className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6"
 
-                  > */}
+                  >
                     <div className="flex justify-end" id="xmark">
                       <button data-testid="close-menu-button" onClick={close}>
                         <XMark />
