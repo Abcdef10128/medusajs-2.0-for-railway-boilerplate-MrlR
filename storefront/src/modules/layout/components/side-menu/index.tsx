07b4350e180023dcbac1550530f8ead306c587fa
Ab Cd <smerch10128@gmail.com>
2025-10-07 22:18:34 +0300

    v12

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -79,7 +79,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         </li>
                       ))}
                     </ul>
-                    <div className="flex flex-col gap-y-6">
+                    {/* <div className="flex flex-col gap-y-6">
                       <div
                         className="flex justify-between"
                         onMouseEnter={toggleState.open}
@@ -102,7 +102,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         © {new Date().getFullYear()} Smerch Store. All rights
                         reserved.
                       </Text>
-                    </div>
+                    </div> */}
                   </div>
                 </Popover.Panel>
               </Transition>
