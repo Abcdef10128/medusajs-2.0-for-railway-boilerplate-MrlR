e69b84284df47ab7d20b73d5d95feb963908025c
Ab Cd <smerch10128@gmail.com>
2025-10-07 23:37:51 +0300

    v12

--- storefront/src/modules/cart/components/item/index.tsx
+++ storefront/src/modules/cart/components/item/index.tsx
@@ -77,7 +77,7 @@ const Item = ({ item, type = "full" }: ItemProps) => {
 
       {type === "full" && (
         <Table.Cell>
-          <div className="flex gap-2 items-center w-15">
+          <div className="flex gap-2 items-center w-10">
             <DeleteButton id={item.id} data-testid="product-delete-button" />
             <CartItemSelect
               value={item.quantity}
