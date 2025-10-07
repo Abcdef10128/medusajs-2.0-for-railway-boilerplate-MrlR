5b83ecd029550fcc31a455f10ed3b023aa18d836
Ab Cd <smerch10128@gmail.com>
2025-10-08 00:13:37 +0300

    v12

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -57,9 +57,9 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                 <Popover.Panel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
                   <div
                     data-testid="nav-menu-popup"
-                    className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6"
+                    className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6 overflow-hidden"
                     style={{
-                      position: 'fixed', // Фиксированная позиция
+                      
 
                     }}
                   >
