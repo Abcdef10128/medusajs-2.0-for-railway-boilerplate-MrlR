67e4163defb544a309986b8f92738d562d0c56f9
Ab Cd <smerch10128@gmail.com>
2025-10-07 23:08:30 +0300

    v12

--- storefront/src/modules/cart/components/item/index.tsx
+++ storefront/src/modules/cart/components/item/index.tsx
@@ -111,7 +111,7 @@ const Item = ({ item, type = "full" }: ItemProps) => {
             <CartItemSelect
               value={item.quantity}
               onChange={(value) => changeQuantity(parseInt(value.target.value))}
-              className="w-1 h-10 p-1"
+              className="w-8 h-10 p-1"
               data-testid="product-select-button"
             >
               {/* TODO: Update this with the v2 way of managing inventory */}
