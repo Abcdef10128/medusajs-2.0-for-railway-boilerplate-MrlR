37c1fa0dbc07a3be7440c952b6aa05085b6fbbf6
Ab Cd <smerch10128@gmail.com>
2025-10-09 22:39:13 +0300

    v12

--- storefront/src/lib/util/money.ts
+++ storefront/src/lib/util/money.ts
@@ -3,8 +3,10 @@ import { isEmpty } from "./isEmpty"
 type ConvertToLocaleParams = {
   amount: number
   currency_code: string
-  minimumFractionDigits?: number
-  maximumFractionDigits?: number
+  // minimumFractionDigits?: number
+  // maximumFractionDigits?: number
+    minimumFractionDigits?: 0
+  maximumFractionDigits?: 0
 
   locale?: string
 }
