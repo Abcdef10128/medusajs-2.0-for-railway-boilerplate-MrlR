eab885b8f8317ca6f8d8ba48eccb3e26850e8338
Ab Cd <smerch10128@gmail.com>
2025-10-10 21:15:07 +0300

    v12

--- storefront/src/lib/util/money.ts
+++ storefront/src/lib/util/money.ts
@@ -34,35 +34,6 @@ type ConvertToLocaleParams = {
 
 //---
 
-export const convertToLocale = ({
-  amount,
-  currency_code,
-  minimumFractionDigits,
-  maximumFractionDigits,
-  locale = "en-US",
-}: ConvertToLocaleParams) => {
-  if (!currency_code || isEmpty(currency_code)) {
-    return amount.toString()
-  }
-
-  // Форматируем цену
-  const formatted = new Intl.NumberFormat(locale, {
-    style: "currency",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-    currency: currency_code,
-  }).format(amount)
-
-  // Заменяем "UAH" на символ гривны "₴"
-  if (currency_code.toUpperCase() === "UAH") {
-    return formatted.replace(/UAH/gi, "₴")
-  }
-
-  return formatted
-}
-
-
-
 // export const convertToLocale = ({
 //   amount,
 //   currency_code,
@@ -74,21 +45,50 @@ export const convertToLocale = ({
 //     return amount.toString()
 //   }
 
-//   // Специальное форматирование для гривны
-//   if (currency_code.toUpperCase() === "UAH") {
-//     const formattedNumber = new Intl.NumberFormat(locale, {
-//       minimumFractionDigits: 0,
-//       maximumFractionDigits: 0,
-//     }).format(amount)
-    
-//     return `${formattedNumber} ₴`
-//   }
-
-//   // Форматируем остальные валюты стандартно
-//   return new Intl.NumberFormat(locale, {
+//   // Форматируем цену
+//   const formatted = new Intl.NumberFormat(locale, {
 //     style: "currency",
 //     minimumFractionDigits: 0,
 //     maximumFractionDigits: 0,
 //     currency: currency_code,
 //   }).format(amount)
+
+//   // Заменяем "UAH" на символ гривны "₴"
+//   if (currency_code.toUpperCase() === "UAH") {
+//     return formatted.replace(/UAH/gi, "₴")
+//   }
+
+//   return formatted
 // }
+
+
+
+export const convertToLocale = ({
+  amount,
+  currency_code,
+  minimumFractionDigits,
+  maximumFractionDigits,
+  locale = "en-US",
+}: ConvertToLocaleParams) => {
+  if (!currency_code || isEmpty(currency_code)) {
+    return amount.toString()
+  }
+
+  // Специальное форматирование для гривны
+  if (currency_code.toUpperCase() === "UAH") {
+    const formattedNumber = new Intl.NumberFormat(locale, {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    }).format(amount)
+    
+    return `${formattedNumber}\u00A0₴`
+  }
+
+  // Форматируем остальные валюты стандартно
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+    currency: currency_code,
+  }).format(amount)
+}
