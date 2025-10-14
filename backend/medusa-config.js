32cf8d4fd4d1f64152c37b0768e4de61e7b8043e
Ab Cd <smerch10128@gmail.com>
2025-10-14 23:08:04 +0300

    v12

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -44,8 +44,7 @@ const medusaConfig = {
     },
     build: {
       rollupOptions: {
-        external: ["@medusajs/dashboard"
-        ]
+        external: ["@medusajs/dashboard"]
       }
     }
   },
