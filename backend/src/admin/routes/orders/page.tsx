607b2a4e4b94796a62888a9865d32a42f5a29f44
Ab Cd <smerch10128@gmail.com>
2025-10-19 14:23:16 +0300

    v12

--- backend/src/admin/routes/orders/page.tsx
+++ backend/src/admin/routes/orders/page.tsx
@@ -1,6 +1,6 @@
 import { defineRouteConfig } from "@medusajs/admin-sdk"
 import { Container } from "@medusajs/ui"
-import OrdersProductsWidget from "admin/widgets/orders-table-widget.tsx" 
+import OrdersProductsWidget from "admin/widgets/orders-table-widget.js" 
 
 const CustomOrdersPage = () => {
   return (
