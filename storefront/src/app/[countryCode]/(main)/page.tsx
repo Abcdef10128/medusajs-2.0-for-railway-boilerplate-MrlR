beecb85405fb977aa996500cf9870e95e59d9227
Abcdef10128 <smerch10128@gmail.com>
2025-11-04 22:16:45 +0200

    v12

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -32,7 +32,7 @@ export default async function Home({
   return (
     <>
       <Hero />
-      <div className="">
+      <div className="mb-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
         </ul>
