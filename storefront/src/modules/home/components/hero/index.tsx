4a0336aaecd19f35cf99b6f4926c50eb951c5cce
Abcdef10128 <smerch10128@gmail.com>
2025-11-10 23:14:21 +0200

    v12

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -67,8 +67,8 @@ const Hero = () => {
         </div>
       </div> */}
 
-      {/* Слайдер */}
-      {/* <div className="mx-auto max-w-[1435px] p-5">
+      Слайдер
+      <div className="mx-auto max-w-[1435px] p-5">
         <Carousel setApi={setApi} className="w-full">
           <CarouselContent>
             {Array.from({ length: 5 }).map((_, index) => (
@@ -87,10 +87,10 @@ const Hero = () => {
         <div className="text-muted-foreground py-2 text-center text-sm">
           Slide {current} of {count}
         </div>
-      </div> */}
+      </div>
 
-      {/* Секция товаров */}
-      {/* <div className="py-12 content-container">
+      Секция товаров
+      <div className="py-12 content-container">
         <div className="mb-8 text-center">
           <Heading level="h2" className="text-2xl-semi">
             Всі продукти
@@ -104,7 +104,7 @@ const Hero = () => {
             countryCode={"ua"}
           />
         </Suspense>
-      </div> */}
+      </div>
     </div>
   )
 }
