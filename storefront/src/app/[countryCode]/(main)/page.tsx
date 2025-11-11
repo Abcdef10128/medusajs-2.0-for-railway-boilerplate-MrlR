2d064943ef5e6c26e40ef855c120d5080090ea8f
Abcdef10128 <smerch10128@gmail.com>
2025-11-12 00:06:44 +0200

    v12

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -32,15 +32,15 @@ export default async function Home({
   return (
     <>
       <Hero />
-      {/* <div className="mb-8 px-4">
-        <ul className="space-y-2 flex items-center max-w-[1392px]">
+      <div className="mb-8 px-4">
+        <ul className="space-y-2 flex items-center max-w-[1392px] mx-auto">
           {collections.map((collection) => (
             <li key={collection.id} className=" h-12 px-3 border rounded hover:bg-gray-50">
               {collection.title}
             </li>
           ))}
         </ul>
-      </div> */}
+      </div>
       <div className="mb-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
