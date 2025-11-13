951d7196d881c383d1c18b40cf3e9a1ecb369bd5
Abcdef10128 <smerch10128@gmail.com>
2025-11-13 23:42:24 +0200

    v12

--- storefront/src/modules/products/components/product-actions/mobile-actions.tsx
+++ storefront/src/modules/products/components/product-actions/mobile-actions.tsx
@@ -127,22 +127,20 @@ const MobileActions: React.FC<MobileActionsProps> = ({
             </div> */}
                               
               {product.options?.length === 1 && ( 
-                  <div className="grid grid-cols-2 w-full gap-x-4">
-                <Button
-                  onClick={open}
-                  variant="secondary"
-                  className="w-full"
-                  data-testid="mobile-actions-button"
-                >
-                  <div className="flex items-center justify-between w-full">
-                    <span>
-                      {variant
-                        ? Object.values(options).join(" / ")
-                        : "Вибрати опції"}
-                    </span>
-                    <ChevronDown />
-                  </div>
-                </Button>
+                  <div className="w-full gap-x-4">
+              <Button
+                onClick={handleAddToCart}
+                disabled={!inStock || !variant}
+                className="w-full"
+                isLoading={isAdding}
+                data-testid="mobile-cart-button"
+              >
+                {!variant
+                  ? "Виберіть варіант"
+                  : !inStock
+                  ? "Закінчився"
+                  : "Додати в кошик"}
+              </Button>
                 </div>
               ) }
 
