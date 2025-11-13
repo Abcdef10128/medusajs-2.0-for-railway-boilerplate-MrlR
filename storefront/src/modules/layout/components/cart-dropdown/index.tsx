0e02921e5f35ecb2afa4a715e52b14d931ad53c2
Abcdef10128 <smerch10128@gmail.com>
2025-11-14 00:01:49 +0200

    v12

--- storefront/src/modules/layout/components/cart-dropdown/index.tsx
+++ storefront/src/modules/layout/components/cart-dropdown/index.tsx
@@ -139,10 +139,10 @@ const CartDropdown = ({
                         <div className="flex flex-col justify-between flex-1">
                           <div className="flex flex-col flex-1">
                             <div className="flex items-start justify-between">
-                              <div className="flex flex-col overflow-ellipsis whitespace-nowrap mr-4 w-full">
+                              <div className="flex flex-col overflow-ellipsis  mr-4 w-full  flex-1 min-w-0">
                               {/* <div className="flex flex-col overflow-ellipsis whitespace-nowrap mr-4 w-[180px]"> */}
 
-                                <h3 className="text-base-regular overflow-hidden text-ellipsis">
+                                <h3 className="text-base-regular overflow-hidden text-ellipsis whitespace-nowrap">
                                   <LocalizedClientLink
                                     href={`/products/${item.variant?.product?.handle}`}
                                     data-testid="product-link"
